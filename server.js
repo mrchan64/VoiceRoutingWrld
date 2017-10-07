0200ade432f5984f739f5c68e80b0d7036c1e4cb
ascasc <amarinderchahal@gmail.com>
2017-10-07 10:19:10 -0700

    By Matthew's request

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var S2T = require('./lib/speechToText'),
     TSA = require('./lib/textSiftAlgo');
     EXS = require('./lib/expressServer')
 
-/*var mic = new S2T.MicrophoneProcess();
+var mic = new S2T.MicrophoneProcess();
 console.log('started')
 
 setTimeout(function(){
   console.log('ending')
   mic.end(console.log);
-}, 5000);*/
+}, 5000);
 
 //test the sifting algorithm here
-TSA.findCommand("");
+//TSA.findCommand("");
